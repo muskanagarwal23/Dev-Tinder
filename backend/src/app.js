5a72646a7c77de2635d3fd48609456e7ba5c2390
muskanagarwal23 <muskaanagarwal2591@gmail.com>
2025-09-14 14:37:34 +0530

    created the express server

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("hello");
+})
+
+app.get("/home", (req,res) => {
+    res.send("hello home");
+})
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+    
+});
